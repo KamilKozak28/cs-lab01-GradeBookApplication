316ed884eee826fa677af07b5b1cdbc5cf01425b
Kamil Kozak <Kamil.Kozak1@microsoft.wsei.edu.pl>
2023-04-01 18:59:16 +0200

    10 test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook:BaseGradeBook
+    internal class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
@@ -14,11 +14,11 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException();
             }
-             else
+            else
             {
                 // continue here  Override `RankedGradeBook`'s `GetLetterGrade` method 
                 double[] table = new double[Students.Count()];
@@ -62,6 +62,17 @@ namespace GradeBook.GradeBooks
             }
         }
 
-        
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
     }
 }
